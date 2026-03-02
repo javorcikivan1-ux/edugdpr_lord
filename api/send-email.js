86d093df747b5f892b5c72b776f155bc98ea36cc
Ivan Javorcik <javorcik.ivan1@gmail.com>
2026-03-02 21:49:56 +0100

    fix esm resend

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
-const { Resend } = require("resend");
+import { Resend } from "resend";
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -29,4 +29,4 @@ module.exports = async function handler(req, res) {
 
   }
 
-};
+}
